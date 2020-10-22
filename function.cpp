2c1bf9b70182920fdf7e1392a8d9ae228f3bc34b
Marko Kuburić <44305151+marko-kuburic@users.noreply.github.com>
2020-10-22 17:06:10 +0200

    Comments
    Adding comments to functions and renaming the last function.

--- function.cpp
+++ function.cpp
@@ -2,17 +2,20 @@
 
 using namespace std;
 
+// returns x to the power of n (x^n)
 int pow(int x, int n)
 {
     return (n == 1) ? x : x * pow(x, n-1);
 }
 
+// returns factorial of n (n!)
 int factorial(int n)
 {
     return (n == 1) ? 1 : n * factorial(n-1);
 }
 
-int eAproximation(int x, int n)
+// returns e to the power of x by expansion to the n-th element
+int mExpansion(int x, int n)
 {
-    return (n == 0) ? 1 : pow(x, n) / factorial(n) + eAproximation(x, n-1);
+    return (n == 0) ? 1 : pow(x, n) / factorial(n) + mExpansion(x, n-1);
 }
